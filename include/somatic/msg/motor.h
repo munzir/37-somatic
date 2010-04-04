6d816ff42c51a89fad900a9abfb42367924df036
Jonathan Scholz <jkscholz@gatech.edu>
2010-04-04 17:30:28 +0000

    Moved all pciod smm functions to somatic
    git-svn-id: file:///home/svn/humanoids/lib/somatic/trunk@3934 4f504ea5-f356-0410-adec-f1b3679f3bb7

--- include/somatic/msg/motor.h
+++ include/somatic/msg/motor.h
@@ -1,12 +1,120 @@
+/* -*- mode: C; c-basic-offset: 2  -*- */
 /*
- * motor.h
+ * Copyright (c) 2009-2010, Georgia Tech Research Corporation
+ * All rights reserved.
  *
- *  Created on: Apr 2, 2010
- *      Author: jscholz
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *     * Neither the name of the Georgia Tech Research Corporation nor
+ *       the names of its contributors may be used to endorse or
+ *       promote products derived from this software without specific
+ *       prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GEORGIA TECH RESEARCH CORPORATION ''AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GEORGIA
+ * TECH RESEARCH CORPORATION BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 
 #ifndef MOTOR_H_
 #define MOTOR_H_
 
+#include "somatic.h"
+#include "somatic/util.h"
+#include "somatic.pb-c.h"
+
+/** \file pciod_smm.h
+ *
+ *  Library to interact with pciod using motor command and state messages
+ *
+ *  \author Jon Scholz
+ */
+
+
+/* Memory Management Functions */
+
+/// Allocate a Somatic__MotorCmd message
+Somatic__MotorCmd *somatic_motorcmd_alloc(size_t n_modules);
+
+/// Allocate a Somatic__MotorState message
+Somatic__MotorState *somatic_motorstate_alloc(size_t n_modules);
+
+/// Free memory allocated by somatic_motorcmd_alloc
+int somatic_motorcmd_free(Somatic__MotorCmd *msg);
+
+/// Free memory allocated by somatic_motorstate_alloc
+int somatic_motorstate_free(Somatic__MotorState *msg);
+
+
+/* Main motor message commands */
+
+/**
+ * Generate a motor command and publish on specified channel
+ * \note pciod will update the state channel after issuing this command
+ *
+ * @param chan The channel to issue motor commands
+ * @param values An array of motor values to write
+ * @param n_modules The number of modules, and size of the values array
+ * @param cmd_type The motor command being issued (current | velocity | position)
+ *
+ * \pre An initialized motor channel
+ * \post Values are written through the ACH channel to the modules
+ */
+int somatic_generate_motorcmd(ach_channel_t *chan, double *values, size_t n_modules, Somatic__MotorParam cmd_type);
+
+/**
+ *  Issues a motor state query request (not implemented yet)
+ *
+ *  @param cmd_chan The channel to issue motor commands
+ *  @param n_modules The number of modules
+ */
+//void somatic_request_motor_state_update(ach_channel_t *cmd_chan, size_t n_modules);
+
+
+/* Message IO */
+
+/// Publish command message on specified channel
+int somatic_motorcmd_publish(Somatic__MotorCmd *msg, ach_channel_t *chan);
+
+/// Publish state message on specified channel
+int somatic_motorstate_publish(Somatic__MotorState *msg, ach_channel_t *chan);
+
+/**
+ * Declare a receive function for motor command message type
+ */
+SOMATIC_DEC_WAIT_LAST_UNPACK(somatic_motorcmd_receive,
+							somatic__motor_cmd,
+							Somatic__MotorCmd);
+
+/**
+ * Declare a receive function for motor statemessage type
+ */
+SOMATIC_DEC_WAIT_LAST_UNPACK(somatic_motorstate_receive,
+							somatic__motor_state,
+							Somatic__MotorState);
+
+/// Print the contents of a Somatic__MotorCmd message
+void somatic_motorcmd_print(Somatic__MotorCmd *msg);
+
+/// Print the contents of a Somatic__MotorState message
+void somatic_motorstate_print(Somatic__MotorState *msg);
 
 #endif /* MOTOR_H_ */
---
+++ msg/motor.c
@@ -0,0 +1,178 @@
+/** \file pciod_ssm.c
+ *
+ *  Library to interact with pciod using motor command and state messages
+ *
+ *  \author Jon Scholz
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdint.h>
+
+#include <somatic.h>
+#include <ach.h>
+
+#include <somatic/util.h>
+#include <somatic.pb-c.h>
+
+#include "somatic/msg/base.h"
+#include "somatic/msg/motor.h"
+
+/*
+ * Allocate a Somatic__MotorCmd message
+ */
+Somatic__MotorCmd *somatic_motorcmd_alloc(size_t n_modules)
+{
+	Somatic__MotorCmd *msg = SOMATIC_NEW(Somatic__MotorCmd);
+	somatic__motor_cmd__init(msg);
+
+	msg->values = somatic_vector_alloc(n_modules);
+
+	return (msg);
+}
+
+/*
+ * Allocate a Somatic__MotorState message
+ */
+Somatic__MotorState *somatic_motorstate_alloc(size_t n_modules)
+{
+	Somatic__MotorState *msg = SOMATIC_NEW(Somatic__MotorState);
+	somatic__motor_state__init(msg);
+
+	msg->position = somatic_vector_alloc(n_modules);
+	msg->velocity = somatic_vector_alloc(n_modules);
+
+	return (msg);
+}
+
+/*
+ * Free dynamically allocated memory from MotorCmd message
+ */
+int somatic_motorcmd_free(Somatic__MotorCmd *msg)
+{
+	somatic_vector_free(msg->values);
+	free(msg);
+
+	return(0);
+}
+
+/*
+ * Free dynamically allocated memory from MotorState message
+ */
+int somatic_motorstate_free(Somatic__MotorState *msg)
+{
+	somatic_vector_free(msg->position);
+	somatic_vector_free(msg->velocity);
+	free(msg);
+
+	return(0);
+}
+
+// Generate a motor command and publish on specified channel
+int somatic_generate_motorcmd(ach_channel_t *chan, double *values, size_t n_modules, Somatic__MotorParam cmd_type)
+{
+	Somatic__MotorCmd msg;
+	somatic__motor_cmd__init(&msg);
+
+	msg.has_param = 1;
+	msg.param = cmd_type;
+	msg.values = SOMATIC_NEW(Somatic__Vector);
+	somatic__vector__init(msg.values);
+	msg.values->data = values;
+	msg.values->n_data = n_modules;
+
+	return somatic_motorcmd_publish(&msg, chan);
+}
+
+// Issues a motor state query request
+//void somatic_request_motor_state_update(ach_channel_t *cmd_chan, size_t n_modules)
+//{
+/*
+ * TODO: implement this (requires an addition to the motor command message to
+ * include whatever CAN message type that is an ack only.
+ */
+//}
+
+// Publish command message on specified channel
+int somatic_motorcmd_publish(Somatic__MotorCmd *msg, ach_channel_t *chan)
+{
+	/* The type parameter (2) is the PREFIX for the appropriate libsomatic
+	 * protobuf functions, not the type itself
+	 */
+	int r = SOMATIC_PACK_SEND( chan, somatic__motor_cmd, msg );
+	somatic_hard_assert( ACH_OK == r, "Failed to send message: %s\n", ach_result_to_string( r ) );
+
+	return(r);
+}
+
+// Publish MotorState message on specified channel
+int somatic_motorstate_publish(Somatic__MotorState *msg, ach_channel_t *chan)
+{
+	/* The type parameter (2) is the PREFIX for the appropriate libsomatic
+	 * protobuf functions, not the type itself
+	 */
+	int r = SOMATIC_PACK_SEND( chan, somatic__motor_state, msg );
+	somatic_hard_assert( ACH_OK == r, "Failed to send message: %s\n", ach_result_to_string( r ) );
+
+	return(r);
+}
+
+/**
+ * Define a receive function for motor command message type
+ */
+SOMATIC_DEF_WAIT_LAST_UNPACK(somatic_motorcmd_receive,
+							somatic__motor_cmd,
+							Somatic__MotorCmd);
+
+/**
+ * Define a receive function for motor statemessage type
+ */
+SOMATIC_DEF_WAIT_LAST_UNPACK(somatic_motorstate_receive,
+							somatic__motor_state,
+							Somatic__MotorState);
+
+// Print the contents of a Somatic__MotorCmd message
+void somatic_motorcmd_print(Somatic__MotorCmd *msg)
+{
+	if (msg->param == SOMATIC__MOTOR_PARAM__MOTOR_CURRENT)
+		fprintf(stdout, "current mode \n");
+	else if (msg->param == SOMATIC__MOTOR_PARAM__MOTOR_VELOCITY)
+		fprintf(stdout, "velocity mode\n");
+	else if (msg->param == SOMATIC__MOTOR_PARAM__MOTOR_POSITION)
+		fprintf(stdout, "position mode\n");
+	else
+		fprintf(stdout, "unrecognized parameter \n");
+
+	size_t i;
+	fprintf(stdout, "Position: ");
+	for (i=0; i < msg->values->n_data; ++i)
+		fprintf(stdout, "% 1.2lf::", msg->values->data[i]);
+	fprintf(stdout, "\n");
+}
+
+// Print the contents of a Somatic__MotorState message
+void somatic_motorstate_print(Somatic__MotorState *msg)
+{
+	size_t i;
+	fprintf(stdout, "Position: ");
+	for (i=0; i<msg->position->n_data; ++i)
+		fprintf(stdout, "% 1.2lf::", msg->position->data[i]);
+	fprintf(stdout, "\n");
+	fprintf(stdout, "Velocity: ");
+	for (i=0; i<msg->velocity->n_data; ++i)
+		fprintf(stdout, "% 1.2lf::", msg->velocity->data[i]);
+	fprintf(stdout, "\n");
+	/*
+	 * TODO: fix this motor status to be an array of enums
+	 * it's dumb to have a single status for the entire pcio group
+	 */
+	if (msg->has_status) {
+		if (msg->status == SOMATIC__MOTOR_STATUS__MOTOR_OK)
+			fprintf(stdout, "Motor okay!\n");
+		else if (msg->status == SOMATIC__MOTOR_STATUS__MOTOR_FAIL)
+			fprintf(stdout, "Motor failure!\n");
+	}
+}
+
