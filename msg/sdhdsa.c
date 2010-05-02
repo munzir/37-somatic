b4273fc9ff0be076f6a529e08e1a21bf99037edb
Hyun-Soo Yi <hyi3@gatech.edu>
2010-05-02 12:17:47 +0000

    Changed sdhsda functions. sdhsda functions are similar to other msg functions.
    git-svn-id: file:///home/svn/humanoids/lib/somatic/trunk@4168 4f504ea5-f356-0410-adec-f1b3679f3bb7

--- msg/sdhdsa.c
+++ msg/sdhdsa.c
@@ -5,46 +5,44 @@
  *      Author: Hyun-Soo Yi
  */
 
-#include <argp.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 #include <stdint.h>
 
-#include <somatic.h>
 #include <ach.h>
 
-#include <somatic/util.h>
-#include <somatic.pb-c.h>
-
-
+#include "somatic/msg/base.h"
 #include "somatic/msg/sdhdsa.h"
 //#include "include/dsawrap.h"
 
 /*
  * Allocate a Somatic__Joystick message
  */
-int somatic_dsa_allocate_msg(Somatic__Dsa *msg, size_t n_data) {
+Somatic__Dsa *somatic_dsa_alloc(size_t n_data) {
+
+	Somatic__Dsa *msg = SOMATIC_NEW(Somatic__Dsa);
 	somatic__dsa__init(msg);
 
-	msg->output = SOMATIC_NEW(Somatic__Ivector);
-	somatic__ivector__init(msg->output);
-	msg->output->data = SOMATIC_NEW_AR(int64_t, n_data);
-	msg->output->n_data = (size_t) n_data;
+	msg->output = somatic_ivector_alloc(n_data);
 
-	//TODO: add return code
+	return (msg);
+}
+/**
+ * Free memory allocated to Somatic__Joystick msg
+ */
+int somatic_dsa_free(Somatic__Dsa *msg) {
+	somatic_ivector_free(msg->output);
+	free(msg);
+
 	return (0);
 }
 
-int somatic_dsa_free_msg(Somatic__Dsa *msg) {
-	free(msg->output->data);
-	free(msg->output);
-	//free(msg);
 
-	return (0);
-}
-
+/**
+ * Publish a Somatic__Joystick message on specified channel
+ */
 int somatic_dsa_publish(Somatic__Dsa *msg, ach_channel_t *chan) {
 	int r = SOMATIC_PACK_SEND( chan, somatic__dsa, msg );
 	somatic_hard_assert(ACH_OK == r, "Failed to send message: %s\n",
@@ -53,10 +51,13 @@ int somatic_dsa_publish(Somatic__Dsa *msg, ach_channel_t *chan) {
 	return (r);
 }
 
-// Defines a receive function
-SOMATIC_DEF_WAIT_LAST_UNPACK(somatic_dsa_receive,
-							somatic__dsa,
-							Somatic__Dsa);
+//Already defined in sdhdsa.h file. Do I have to redefine?
+///*
+// * Defines a receive function
+// */
+//SOMATIC_DEF_WAIT_LAST_UNPACK(somatic_dsa_receive,
+//							somatic__dsa,
+//							Somatic__Dsa);
 
 /*
  * Print the contents of a Somatic__Dsa message
