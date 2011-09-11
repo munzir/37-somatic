42e048e6429980d49271ae54497454987102370a
Neil T. Dantam <ntd@gatech.edu>
2011-09-11 20:35:28 +0000

    date fix
    git-svn-id: file:///home/svn/humanoids/src/common/lib/somatic@6104 4f504ea5-f356-0410-adec-f1b3679f3bb7

--- include/somatic.h
+++ include/somatic.h
@@ -72,7 +72,7 @@
  * \author Under Direction of Professor Mike Stilman
  *
  * \copyright
- * Copyright (c) 2008-2011, Georgia Tech Research Corporation.
+ * Copyright (c) 2009-2011, Georgia Tech Research Corporation.
  * All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or
