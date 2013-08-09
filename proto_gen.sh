f9a9b397ba8d2daa3f3e306834644d607e18610f
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2013-08-09 19:26:03 -0400

    fix proto_get.sh - should have been moving to .c not .cpp

--- proto_gen.sh
+++ proto_gen.sh
@@ -6,9 +6,9 @@
 echo "compiling somatic.proto"
 protoc-c --c_out=. proto/somatic.proto
 
-mv -v proto/somatic.pb-c.c src/somatic.pb-c.cpp
+mv -v proto/somatic.pb-c.c src/somatic.pb-c.c
 mv -v proto/somatic.pb-c.h include/somatic.pb-c.h
-sed -i -e "s/#include \"proto\\/somatic.pb-c.h\"/#include \"somatic.pb-c.h\"/" src/somatic.pb-c.cpp
+sed -i -e "s/#include \"proto\\/somatic.pb-c.h\"/#include \"somatic.pb-c.h\"/" src/somatic.pb-c.c
 echo "done."
 
 # should add include guard here
